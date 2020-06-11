c6fd552c428c8f2f4d679f60fad9a9bf4ce05a3e
erenaykrcn <61312469+erenaykrcn@users.noreply.github.com>
2020-06-11 11:03:04 +0200

    UPDATE SETTINGS APP

--- settings/models.py
+++ settings/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Setting(models.Model):
-	period = models.CharField(null=True, max_length=1000000000000, default="10")
-	commissionPercentage = models.CharField(null=True, max_length=1000000000000, default="2")
-	minimumCommission = models.CharField(null=True, max_length=1000000000000, default="2")
-	buysellMargin = models.CharField(null=True, max_length=1000000000000, default="2")
-	surcharge = models.CharField(null=True, max_length=1000000000000, default="2")
+	period = models.TextField(null=True)
+	commissionPercentage = models.TextField(null=True)
+	minimumCommission = models.TextField(null=True)
+	buysellMargin = models.TextField(null=True)
+	surcharge = models.TextField(null=True)
